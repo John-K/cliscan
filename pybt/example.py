805ed67bc20768de99cc82c61bde8a79368aaa0e
Andre Pang <andre@pixar.com>
2013-08-15 17:06:34 -0700

    Rejiggered hello.bt API to use delegate-style callbacks instead of function callbacks

--- pybt/example.py
+++ pybt/example.py
@@ -1,46 +1,99 @@
 #!/usr/bin/env python
 
-import hello.bt
+import logging as log
+import sys
 
-BT_SERVICE_BATTERY = '180f'
-BT_CHARACTERISTIC_BATTERY_LEVEL = '2a19'
+from Cocoa import *
+from IOBluetooth.CoreBluetooth import *
 
-"""The intention is to make a slightly nicer Python API for people to
-use. The Objective-C madness of delegates and callbacks don't
-translate very well to Python, and using big fat objects for the
-simple tasks that we need is kinda overkill.  So the idea's to expose
-just a few functions in the hello.bt module that are a little more
-Pythonic.
+import hello.bt as bt
 
-"""
+BATTERY_SERVICE = '180f'
+BATTERY_LEVEL_CHARACTERISTIC = '2a19'
 
-def peripheral_ready(peripheral):
-    # This function gets called when a new peripheral is detected. The
-    # idea for this is to return immediately if you don't detect the
-    # peripheral that you're interested in; if the peripheral _is_ the
-    # one you're interested in, well, do something with it here :).
+# CoreBluetooth programming guide (Performing Common Central Role Tasks):
+# http://developer.apple.com/library/ios/documentation/NetworkingInternetWeb/Conceptual/CoreBluetooth_concepts/PerformingCommonCentralRoleTasks/PerformingCommonCentralRoleTasks.html
+#
+# CoreBluetooth framework reference:
+# http://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CoreBluetooth_Framework/_index.html
+#
+# CBPeripheral:
+# http://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBPeripheral_Class/translated_content/CBPeripheral.html
+#
+# CBPeripheralDelegate:
+# https://developer.apple.com/library/mac/documentation/CoreBluetooth/Reference/CBPeripheralDelegate_Protocol/translated_content/CBPeripheralDelegate.html
+#
+# CBService:
+# http://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBService_Class/translated_content/CBService.html
+#
+# CBCharacteristic:
+# http://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBCharacteristic_Class/translated_content/CBCharacteristic.html
 
-    try:
-        # Here, service & characteristic are full-blown CoreBluetooth
-        # CBService and CBPeripheral PyObjC objects. You can call any
-        # methods on them, so see the docs at
-        # <http://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CoreBluetooth_Framework/_index.html>
-        # for the methods that you can use on them. If you're not
-        # familiar with PyObjC, the introduction document at
-        # <http://pythonhosted.org/pyobjc/core/intro.html#first-steps>
-        # shows you how to call methods.
+class BandReadBatteryDelegate(bt.AppDelegate):
+    # This is an example delegate class that you should implement.  See the docstrings for the method definitios in the AppDelegate class for their specification.
 
-        # You can call methods to read/write/notify to
-        # characteristics, but there's no feedback that you get from
-        # that yet, so it's probably not useful yet.
+    def shouldUsePeripheral(self, peripheral):
+        return peripheral.name() == 'Band (DFU Mode)'
 
-        service = peripheral[BT_SERVICE_BATTERY]
-        characteristic = service[BT_CHARACTERISTIC_BATTERY_LEVEL]
+    def servicesWanted(self, peripheral):
+        if self.shouldUsePeripheral(peripheral):
+            return [BATTERY_SERVICE]
+        else:
+            return None
 
-        print "Found characteristic %s" % characteristic.UUID().description()
+    def characteristicsWanted(self, service):
+        if bt.UUIDStringForBluetoothObject(service) == BATTERY_SERVICE:
+            return [BATTERY_LEVEL_CHARACTERISTIC]
+        else:
+            return None
 
-    except KeyError:
-        # service/characteristic hasn't been scanned on the device yet, so do nothing
-        return
+    def characteristicDiscovered(self, characteristic):
+        log.debug('Found characteristic %s for service %s' % (
+            bt.UUIDStringForBluetoothObject(characteristic),
+            bt.UUIDStringForBluetoothObject(characteristic.service())))
 
-hello.bt.find_peripheral('LightBlue', peripheral_ready)
+        if bt.isCharacteristic(characteristic, BATTERY_LEVEL_CHARACTERISTIC, BATTERY_SERVICE):
+            print 'Starting read from %s%s' % (BATTERY_SERVICE, BATTERY_LEVEL_CHARACTERISTIC)
+            characteristic.service().peripheral().readValueForCharacteristic_(characteristic)
+
+    def characteristicDidRead(self, characteristic):
+        if bt.isCharacteristic(characteristic, BATTERY_LEVEL_CHARACTERISTIC, BATTERY_SERVICE):
+            print "[%s%s]: %s" % (BATTERY_SERVICE, BATTERY_LEVEL_CHARACTERISTIC, characteristic.value())
+            print "Done. Press Ctrl-Z then 'kill %1' to kill this thing."
+
+class CyclingSpeedAndCadenceWriteDelegate(bt.AppDelegate):
+    # An example delegate class for how to write to a characteristic.
+
+    SERVICE = '1816'
+    CHARACTERISTIC = '2a55'
+
+    def shouldUsePeripheral(self, peripheral):
+        return peripheral.name() == 'LightBlue'
+
+    def servicesWanted(self, peripheral):
+        if self.shouldUsePeripheral(peripheral):
+            return [self.SERVICE]
+        else:
+            return None
+
+    def characteristicsWanted(self, service):
+        if bt.UUIDStringForBluetoothObject(service) == self.SERVICE:
+            return [self.CHARACTERISTIC]
+        else:
+            return None
+
+    def characteristicDiscovered(self, characteristic):
+        if bt.isCharacteristic(characteristic, self.CHARACTERISTIC, self.SERVICE):
+            print 'Starting write to %s%s' % (self.SERVICE, self.CHARACTERISTIC)
+            characteristic.service().peripheral().writeValue_forCharacteristic_type_(
+                NSString.stringWithString_('Awesomesauce').dataUsingEncoding_(NSUTF8StringEncoding),
+                characteristic,
+                CBCharacteristicWriteWithoutResponse)
+
+    def characteristicDidWrite(self, characteristic):
+        if bt.isCharacteristic(characteristic, self.CHARACTERISTIC, self.SERVICE):
+            print "Updated %s%s to %s" % (self.SERVICE, self.CHARACTERISTIC, characteristic.value())
+
+if __name__ == '__main__':
+    bt.go(BandReadBatteryDelegate)
+    # bt.go(CyclingSpeedAndCadenceWriteDelegate)
--- pybt/hello/bt/__init__.py
+++ pybt/hello/bt/__init__.py
@@ -16,14 +16,66 @@ Cocoa = objc.initFrameworkWrapper(
 NSApp = None
 
 class AppDelegate(NSObject):
+
+    allPeripherals = []
     peripherals = []
 
-    # NSApplicationDelegate
+    # Callbacks. You probably want to override these in a subclass.
+
+    def shouldUsePeripheral(self, peripheral):
+        """Return True if you are interested in this peripheral. If you are
+        not interested in this peripheral, return False, and you will
+        not receive any callbacks for it.
+        """
+
+        log.debug('Found peripheral %s, scanning all services...' % (peripheral.name()))
+
+        return True
+
+    def servicesWanted(self, peripheral):
+        """Return a list of UUID strings (lowercase hex), e.g. ['180f',
+        '180a', '180b'] that you are interested in.  You will receive
+        callbacks only for the services that you've speciefied here.
+        Return None if you want all services.
+        """
+
+        return None
+
+    def characteristicsWanted(self, service):
+        """Return a list of UUID strings (lowercase hex) of characteristics
+        that you're interested in for the given service, e.g. ['180f',
+        '180a', '180b'].  Return None if you want all services.
+        """
+
+        log.debug('Found service %s, scanning all characteristics...', UUIDStringForBluetoothObject(service))
+
+        return None
+
+    def characteristicDiscovered(self, characteristic):
+        """This method will get called when a characteristic you're interested in gets discovered."""
+
+        log.debug('Found characteristic %s, scanning all descriptors...', UUIDStringForBluetoothObject(characteristic))
+        characteristic().service().peripheral().discoverDescriptorsForCharacteristic_(characteristic)
+
+    def characteristicDidRead(self, characteristic):
+        """This method will get called when a characteristic gets read."""
+        raise NotImplementedError
+
+    def characteristicDidWrite(self, characteristic):
+        """This method will get called when a characteristic gets written to."""
+        raise NotImplementedError
+
+    def errorHandler(self, error):
+        """Any errors that occur will be passed to this callback."""
+        log.error(error)
+
+    # NSApplication delegates
 
     def applicationDidFinishLaunching_(self, notification):
         self.manager = CBCentralManager.alloc().initWithDelegate_queue_(self, None)
 
-    # CBCentralManagerDelegate
+    # CBCentralManagerDelegate methods. You shouldn't need to override
+    # these. If you do, let me know and I'll add it to the more standard Pythoin callbacks.
 
     def centralManagerDidUpdateState_(self, central):
         assert central.state() == CBCentralManagerStatePoweredOn
@@ -31,40 +83,48 @@ class AppDelegate(NSObject):
 
     def centralManager_didConnectPeripheral_(self, central, peripheral):
         peripheral.setDelegate_(self)
-        peripheral.discoverServices_(None)
+        peripheral.discoverServices_(UUIDStringsToUUIDs(self.servicesWanted(peripheral)))
 
-    def centralManager_didDisconnectPeripheral_error_(self, central, peripheral, error): #
-        pass
+    def centralManager_didDisconnectPeripheral_error_(self, central, peripheral, error):
+        self.manager.scanForPeripheralsWithServices_options_(None, None)
 
     def centralManager_didDiscoverPeripheral_advertisementData_RSSI_(self, central, peripheral, advertisementData, rssi):
-        self.peripherals.append(peripheral)
+        if peripheral in self.allPeripherals:
+            return
 
-        if peripheral.name() == self.peripheralName:
-            log.debug("Found %s" % peripheral.name())
-            #self.manager.stopScan()
+        self.allPeripherals.append(peripheral)
+
+        if self.shouldUsePeripheral(peripheral):
+            self.peripherals.append(peripheral)
             self.manager.connectPeripheral_options_(peripheral, None)
-        elif peripheral.name is not None:
-            log.debug("Found %s (%s), will keep scanning..." % (peripheral.UUID(), peripheral.name()))
+        else:
+            log.debug("Ignoring peripheral %s (%s)" % (peripheral.UUID().description(), peripheral.name()))
 
     def centralManager_didFailToConnectPeripheral_error_(self, central, peripheral, error):
+        log.debug('centralManager_didFailToConnectPeripheral_error_')
         pass
 
     def centralManager_didRetrieveConnectedPeripherals_(self, central, peripherals):
+        log.debug('centralManager_didRetrieveConnectedPeripherals_')
         pass
 
     def centralManager_didRetrievePeripherals_(self, central, peripherals):
+        log.debug('centralManager_didRetrievePeripherals_')
         pass
 
     # CBPeripheralDelegate
 
     def peripheral_didDiscoverServices_(self, peripheral, error):
+        self.manager.stopScan()
+
         for service in peripheral.services():
-            peripheral.discoverCharacteristics_forService_(None, service)
+            peripheral.discoverCharacteristics_forService_(
+                UUIDStringsToUUIDs(self.characteristicsWanted(service)),
+                service)
 
     def peripheral_didDiscoverCharacteristicsForService_error_(self, peripheral, service, error):
         for characteristic in service.characteristics():
-            peripheral.discoverDescriptorsForCharacteristic_(characteristic)
-            self.readyCallback(peripheral)
+            self.characteristicDiscovered(characteristic)
 
     def peripheral_didDiscoverDescriptorsForCharacteristic_error_(self, peripheral, characteristic, error):
         log.debug('peripheral_didDiscoverDescriptorsForCharacteristic_error_')
@@ -77,13 +137,19 @@ class AppDelegate(NSObject):
         pass
 
     def peripheral_didUpdateValueForCharacteristic_error_(self, peripheral, characteristic, error):
-        pass
+        if error is None:
+            self.characteristicDidRead(characteristic)
+        else:
+            self.errorHandler(error)
 
     def peripheral_didUpdateValueForDescriptor_error_(self, peripheral, descriptor, error):
         pass
 
     def peripheral_didWriteValueForCharacteristic_error_(self, peripheral, characteristic, error):
-        pass
+        if error is None:
+            self.characteristicDidWrite(characteristic)
+        else:
+            self.errorHandler(error)
 
     def peripheral_didWriteValueForDescriptor_error_(self, peripheral, descriptor, error):
         pass
@@ -106,7 +172,30 @@ class AppDelegate(NSObject):
 # Utility functions
 
 def UUIDStringForBluetoothObject(obj):
-    return obj.UUID().data().description()[1:5]
+    """Given a Bluetooth object (e.g. CBCharacteristic, CBService), this
+    returns the UUID for that object as a string (as either a 16-bit
+    or 128-bit UUID).  e.g. '180f'."""
+    return obj.UUID().data().description()[1:-1]
+
+def UUIDStringsToUUIDs(UUIDStrings):
+    """Convert a list of 16-bit UUID strings (e.g. ['180f', '2a99']) to a
+    list of CBUUID objects.  Returns None if passed in None."""
+    if UUIDStrings is None:
+        return None
+    else:
+        return [CBUUID.UUIDWithString_(UUIDString) for UUIDString in UUIDStrings]
+
+def isCharacteristic(characteristic, characteristicUUIDString, serviceUUIDString=None):
+    """Returns True if the CBCharacteristic object has a UUID equal to the
+    given characteristicUUIDString, and optionally if the object's service
+    is equal to the given serviceUUIDString."""
+    if UUIDStringForBluetoothObject(characteristic) != characteristicUUIDString:
+        return False
+
+    if serviceUUIDString is not None and UUIDStringForBluetoothObject(characteristic.service()) != serviceUUIDString:
+        return False
+
+    return True
 
 # CBPeripheral additions
 
@@ -120,6 +209,7 @@ def peripheralGetItem(self, key):
             return service
 
     raise KeyError
+
 CBPeripheral.__getitem__ = peripheralGetItem
 
 # CBService additions
@@ -137,16 +227,12 @@ def serviceGetItem(self, key):
 
 CBService.__getitem__ = serviceGetItem
 
-def find_peripheral(peripheralName, readyCallback):
-    try:
-        appDelegate = AppDelegate.alloc().init()
-        appDelegate.peripheralName = peripheralName
-        appDelegate.readyCallback = readyCallback
+# go
 
-        global NSApp
-        NSApp = NSApplication.sharedApplication()
-        NSApp.setDelegate_(appDelegate)
-        NSApp.run()
-        #NSRunLoop.mainRunLoop().run()
-    except KeyboardInterrupt:
-        sys.exit(1)
+def go(delegateClass, **kwargs):
+    delegate = delegateClass.alloc().init()
+
+    global NSApp
+    NSApp = NSApplication.sharedApplication()
+    NSApp.setDelegate_(delegate)
+    NSApp.run()
