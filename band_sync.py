48cc32656b3494d50bfb10aa52ad81806b43bd63
Andre Pang <andre.pang@gmail.com>
2014-03-10 17:09:11 -0700

    Remove dead code from band_sync.py

--- band_sync.py
+++ band_sync.py
@@ -24,57 +24,6 @@ PACKET_SIZE = 20
 class ControlCodes(object):
     SEND_SENSOR_DATA = 2
 
-class OpCodes(object):
-    START_DFU = 1
-    INITIALIZE_DFU = 2
-    RECEIVE_FIRMWARE_IMAGE = 3
-    VALIDATE_FIRMWARE_IMAGE = 4
-    ACTIVATE_FIRMWARE_AND_RESET = 5
-    SYSTEM_RESET = 6
-    REQ_PKT_RCPT_NOTIF = 8
-    RESPONSE = 16
-    PKT_RCPT_NOTIF = 17
-
-op_codes = dict(zip(OpCodes.__dict__.values(), OpCodes.__dict__.keys()))
-
-status_code_lookup = {
-    1: "SUCCESS",
-    2: "Invalid State",
-    3: "Not Supported",
-    4: "Data Size Exceeds Limit",
-    5: "CRC Error",
-    6: "Operation Failed"
-}
-
-def uint32_bytearray(value):
-    return bytearray(struct.pack('<I', value))
-
-def uint16_bytearray(value):
-    return bytearray(struct.pack('<H', value))
-
-def validate_dfu_response(data):
-    op_code = data[0]
-
-    if op_code == OpCodes.PKT_RCPT_NOTIF:
-        print('Received PKT_RCPT_NOTIF for %d bytes' % struct.unpack('<H', str(data[0:2])))
-    elif op_code == OpCodes.RESPONSE:
-        request_op_code = int(data[1])
-        request_op_code_text = op_codes.get(request_op_code, '(unknown op code)')
-
-        response_value = int(data[2])
-        response_text = status_code_lookup.get(response_value, '(unknown response value)')
-
-        print('Received response OpCode %s (%d): %s (%d)' % (
-            request_op_code_text,
-            request_op_code,
-            response_text,
-            response_value))
-    else:
-        dataText = ''
-        for c in data:
-            dataText += '%x' % ord(c)
-        print('Received unknown response (%s): %s' % (type(data), dataText))
-
 def main(argv):
     arg_parser = argparse.ArgumentParser(description='Read sensor data from Band and upload it to Ingress server.')
     arg_parser.add_argument(
